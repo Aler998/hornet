e286a4da37bf556c68383423cb3284cb57c03656
Aler998 <alessandro.ercole98@gmail.com>
2025-05-12 12:36:12 +0200

    fixed simplebar + memory limits

--- admin/src/theme/components/scrollbar/scrollbar.tsx
+++ admin/src/theme/components/scrollbar/scrollbar.tsx
@@ -1,11 +1,42 @@
-import SimpleBar from 'simplebar-react';
-import { mergeClasses } from 'minimal-shared/utils';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as React from "react";
+import type { ReactNode, MutableRefObject } from "react";
+import SimpleBarCore from "simplebar-core";
+import type { SimpleBarOptions } from "simplebar-core";
+type RenderFunc = (props: {
+  scrollableNodeRef: MutableRefObject<HTMLElement | undefined>;
+  scrollableNodeProps: {
+    className: string;
+    ref: MutableRefObject<HTMLElement | undefined>;
+  };
+  contentNodeRef: MutableRefObject<HTMLElement | undefined>;
+  contentNodeProps: {
+    className: string;
+    ref: MutableRefObject<HTMLElement | undefined>;
+  };
+}) => ReactNode;
+export interface Props
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, "children">,
+    SimpleBarOptions {
+  children?: ReactNode | RenderFunc;
+  scrollableNodeProps?: {
+    ref?: any;
+    className?: string;
+    [key: string]: any;
+  };
+}
+declare const SimpleBar: React.ForwardRefExoticComponent<
+  Props & React.RefAttributes<SimpleBarCore | null>
+>;
+export default SimpleBar;
 
-import { styled } from '@mui/material/styles';
+import { mergeClasses } from "minimal-shared/utils";
 
-import { scrollbarClasses } from './classes';
+import { styled } from "@mui/material/styles";
 
-import type { ScrollbarProps } from './types';
+import { scrollbarClasses } from "./classes";
+
+import type { ScrollbarProps } from "./types";
 
 // ----------------------------------------------------------------------
 
@@ -26,9 +57,10 @@ export function Scrollbar({
       className={mergeClasses([scrollbarClasses.root, className])}
       sx={[
         {
-          '& .simplebar-wrapper': slotProps?.wrapperSx as React.CSSProperties,
-          '& .simplebar-content-wrapper': slotProps?.contentWrapperSx as React.CSSProperties,
-          '& .simplebar-content': slotProps?.contentSx as React.CSSProperties,
+          "& .simplebar-wrapper": slotProps?.wrapperSx as React.CSSProperties,
+          "& .simplebar-content-wrapper":
+            slotProps?.contentWrapperSx as React.CSSProperties,
+          "& .simplebar-content": slotProps?.contentSx as React.CSSProperties,
         },
         ...(Array.isArray(sx) ? sx : [sx]),
       ]}
@@ -42,19 +74,19 @@ export function Scrollbar({
 // ----------------------------------------------------------------------
 
 const ScrollbarRoot = styled(SimpleBar, {
-  shouldForwardProp: (prop: string) => !['fillContent', 'sx'].includes(prop),
-})<Pick<ScrollbarProps, 'fillContent'>>(({ fillContent }) => ({
+  shouldForwardProp: (prop: string) => !["fillContent", "sx"].includes(prop),
+})<Pick<ScrollbarProps, "fillContent">>(({ fillContent }) => ({
   minWidth: 0,
   minHeight: 0,
   flexGrow: 1,
-  display: 'flex',
-  flexDirection: 'column',
+  display: "flex",
+  flexDirection: "column",
   ...(fillContent && {
-    '& .simplebar-content': {
-      display: 'flex',
-      flex: '1 1 auto',
-      minHeight: '100%',
-      flexDirection: 'column',
+    "& .simplebar-content": {
+      display: "flex",
+      flex: "1 1 auto",
+      minHeight: "100%",
+      flexDirection: "column",
     },
   }),
 }));
