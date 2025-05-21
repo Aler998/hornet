27be6b146ff7f3ef2a9c5684dcc2862148cec417
Aler998 <alessandro.ercole98@gmail.com>
2025-05-21 14:48:34 +0200

    Fetch_Benzina_Cron

--- backend/src/services/FetchCostoBenzina.ts
+++ backend/src/services/FetchCostoBenzina.ts
@@ -6,7 +6,7 @@ import { readFile } from "fs/promises";
 export const jsonDataFile = path.join(
   __dirname,
   "../../",
-  "prezzi-benzina.json",
+  "prezzi-benzina.json"
 );
 
 type PrezzoInfo = {
@@ -24,62 +24,70 @@ type Distributore = {
 };
 
 export const fetchAndCombineCSVData = async (
-  outputPath: string,
+  outputPath: string
 ): Promise<void> => {
+  // da lanciare in caso non funzioni
+  // node -e "require('https').get('https://www.mimit.gov.it', res => console.log(res.statusCode)).on('error', console.error)"
+
   const anagraficaUrl =
     "https://www.mimit.gov.it/images/exportCSV/anagrafica_impianti_attivi.csv";
   const prezziUrl =
     "https://www.mimit.gov.it/images/exportCSV/prezzo_alle_8.csv";
 
-  return await axios.get(anagraficaUrl).then(async (anagrafica) => {
-    await axios.get(prezziUrl).then((prezzi) => {
-      const anagraficaRows = anagrafica.data.split("\n");
-      const prezziRows = prezzi.data.split("\n");
-      const result: Record<string, Distributore> = {};
+  return await axios
+    .get(anagraficaUrl, { timeout: 60000 })
+    .then(async (anagrafica) => {
+      await axios.get(prezziUrl, { timeout: 60000 }).then((prezzi) => {
+        const anagraficaRows = anagrafica.data.split("\n");
+        const prezziRows = prezzi.data.split("\n");
+        const result: Record<string, Distributore> = {};
 
-      for (const row of anagraficaRows) {
-        const columns = row.split(";");
-        if (columns.length >= 9) {
-          const key = columns[0];
-          if (!isNaN(parseInt(key))) {
-            const value = columns.slice(1);
-            result[key] = {
-              gestore: value[1],
-              indirizzo: `${value[4]} ${value[5]} ${value[6]}`,
-              latitudine: value[7],
-              longitudine: value[8],
-              prezzi: {},
-            };
-          }
-        }
-      }
-
-      for (const row of prezziRows) {
-        const columns = row.split(";");
-        if (columns.length >= 4) {
-          const key = columns[0];
-          const carburante = columns[1].toLowerCase();
-          const prezzo = columns[2];
-          const self = columns[3];
-          const data = columns[4];
-
-          if (result[key]) {
-            const existing = result[key].prezzi[carburante];
-            if (!existing || parseFloat(prezzo) < parseFloat(existing.prezzo)) {
-              result[key].prezzi[carburante] = { prezzo, self, data };
+        for (const row of anagraficaRows) {
+          const columns = row.split(";");
+          if (columns.length >= 9) {
+            const key = columns[0];
+            if (!isNaN(parseInt(key))) {
+              const value = columns.slice(1);
+              result[key] = {
+                gestore: value[1],
+                indirizzo: `${value[4]} ${value[5]} ${value[6]}`,
+                latitudine: value[7],
+                longitudine: value[8],
+                prezzi: {},
+              };
             }
           }
         }
-      }
 
-      fs.writeFileSync(outputPath, JSON.stringify(result, null, 2));
+        for (const row of prezziRows) {
+          const columns = row.split(";");
+          if (columns.length >= 4) {
+            const key = columns[0];
+            const carburante = columns[1].toLowerCase();
+            const prezzo = columns[2];
+            const self = columns[3];
+            const data = columns[4];
+
+            if (result[key]) {
+              const existing = result[key].prezzi[carburante];
+              if (
+                !existing ||
+                parseFloat(prezzo) < parseFloat(existing.prezzo)
+              ) {
+                result[key].prezzi[carburante] = { prezzo, self, data };
+              }
+            }
+          }
+        }
+
+        fs.writeFileSync(outputPath, JSON.stringify(result, null, 2));
+      });
     });
-  });
 };
 
 export function getPriceByStationId(
   data: Record<string, Distributore>,
-  id: number,
+  id: number
 ): string {
   const station = data[id];
   const prezzo = station.prezzi["benzina"];
@@ -96,6 +104,16 @@ export function hasFileBeenUpdatedWithin24Hours(filePath: string): boolean {
     return false;
   }
 }
+
+export const checkAndFetchFile = async () => {
+  if (
+    !fs.existsSync(jsonDataFile) ||
+    !hasFileBeenUpdatedWithin24Hours(jsonDataFile)
+  ) {
+    console.log("Updating JSON file...");
+    await fetchAndCombineCSVData(jsonDataFile);
+  }
+};
 
 export const getCurrentBenzinaPrice = async (): Promise<number> => {
   const DISTRIBUTORE_ID = 57411;
